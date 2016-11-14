021b36105e010b9134b76cdf8b47952589d5fff8
sehe <sgheeren@gmail.com>
2016-11-14 23:51:12 +0100

    Added missing explicit instantiation

--- types_def.hpp
+++ types_def.hpp
@@ -35,6 +35,22 @@ BOOST_SPIRIT_DEFINE(
         type
 )
 
+namespace {
+    template <typename Seq, size_t N>
+        using field_at = boost::fusion::basic_iterator<boost::fusion::struct_iterator_tag, boost::fusion::random_access_traversal_tag, Seq, N>;
+    template <typename Seq, size_t N, size_t M>
+        using fields   = boost::fusion::iterator_range<field_at<Seq, N>, field_at<Seq, M> >;
+
+    using Attributes = fields<client::ast::VariableDec, 1, 2>;
+    using Context  = x3::context<x3::skipper_tag, x3::char_class<boost::spirit::char_encoding::ascii, x3::space_tag> const, x3::unused_type>;
+}
+
+template bool parse_rule<std::string::const_iterator, Context, fields<client::ast::VariableDec, 1, 2> >(
+    decltype(type),
+    std::string::const_iterator &, 
+    std::string::const_iterator const &,
+    Context const &,
+    Attributes &);
 
 }}
 
